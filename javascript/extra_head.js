997c5606b9afb7e3c1c37d15bc451522d960f8f6
utsgithub <uts_github@163.com>
2016-09-01 19:02:51 +1000

    Add project files.

---
+++ javascript/extra_head.js
@@ -0,0 +1,18 @@
+﻿
+//todo: Translate to English
+window.onbeforeunload = onbeforeunload_handler;
+function onbeforeunload_handler() {
+    var warning = "确认退出?";
+    return warning;
+}
+$(function () {
+    $(document).keydown(function (event) {
+        if ((event.altKey && event.keyCode == 83)) {
+            //在这里接收的是Alt+S事件,S的ASCII码为83。
+            //alert( $("#btnPart a:not(:hidden)").length );
+            if ($("#btnPart a:not(:hidden)").length == 1) {
+                $("#btnPart a:not(:hidden)").click();
+            }
+        }
+    });
+});
